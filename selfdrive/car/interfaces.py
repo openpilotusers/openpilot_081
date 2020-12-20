adc73f115d008b2b7d28ab54de18e424ae92c84c
SkyGraceHa <gopyeong@naver.com>
2020-12-20 21:27:31 +0900

    Update interfaces.py
    기어 드라이브 모드 오류 삭제

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -99,8 +99,9 @@ class CarInterfaceBase():
       events.add(EventName.doorOpen)
     if cs_out.seatbeltUnlatched:
       events.add(EventName.seatbeltNotLatched)
-    if cs_out.gearShifter != GearShifter.drive and cs_out.gearShifter not in extra_gears and cs_out.cruiseState.enabled:
-      events.add(EventName.wrongGear)
+  # 기어 드라이브 모드 오류 삭제
+    # if cs_out.gearShifter != GearShifter.drive and cs_out.gearShifter not in extra_gears and cs_out.cruiseState.enabled:
+    #   events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
     if not cs_out.cruiseState.available and cs_out.cruiseState.enabled:
