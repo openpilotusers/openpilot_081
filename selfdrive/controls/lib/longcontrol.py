49d5e3eb6ab523fb1f40747e1d51df5fb6df9a0c
Vehicle Researcher <>
2021-01-04 23:23:42 +0900

    openpilot v0.8.1

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -154,17 +154,17 @@ class LongControl():
       if hasLead and radarState.leadOne.status and 3 < dRel <= 35 and output_gb < 0 and vRel < 0 and (CS.vEgo*CV.MS_TO_KPH) <= 65:
         vd_r = (CS.vEgo*CV.MS_TO_KPH)/dRel
         multiplier = max((self.v_pid/(max(v_target_future, 1))), 1)
-        multiplier = min(multiplier, 4)
+        multiplier = min(multiplier, 5)
         if vd_r >= 1.5:
           output_gb *= multiplier
           output_gb = clip(output_gb, -brake_max, gas_max)
         else:
-          output_gb *= 1.4
+          output_gb *= 1.5
       elif hasLead and radarState.leadOne.status and 7 < dRel < 17 and abs(vRel*3.6) > 4 and output_gb > 0 and (CS.vEgo * CV.MS_TO_KPH) < 25:
         output_gb *= 1.3
         output_gb = clip(output_gb, -brake_max, gas_max)
       elif hasLead and radarState.leadOne.status and 4 < dRel < 100 and output_gb < 0:
-        output_gb *= 1.4
+        output_gb *= 1.5
 
       if prevent_overshoot:
         output_gb = min(output_gb, 0.0)
