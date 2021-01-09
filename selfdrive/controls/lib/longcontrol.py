97af2644d1e628cc8439e10bfacef88ed66eb9c6
Vehicle Researcher <>
2021-01-09 15:13:11 +0900

    openpilot v0.8.1

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -157,7 +157,7 @@ class LongControl():
 
       if hasLead and radarState.leadOne.status and 4 < dRel <= 50 and output_gb < 0 and vRel < 0 and (CS.vEgo*CV.MS_TO_KPH) <= 70:
         multiplier = max((self.v_pid/(max(v_target_future, 1))), 1)
-        multiplier = clip(multiplier, 1.1, 2.5)
+        multiplier = clip(multiplier, 1.1, 3)
         output_gb *= multiplier
         output_gb = clip(output_gb, -brake_max, gas_max)
       elif hasLead and radarState.leadOne.status and 4 < dRel <= 50 and output_gb > 0 and vRel < 0 and (CS.vEgo*CV.MS_TO_KPH) <= 70:
@@ -166,7 +166,7 @@ class LongControl():
         output_gb *= 1.1
 
       if hasLead and radarState.leadOne.status and dRel*2 < (CS.vEgo*CV.MS_TO_KPH) and output_gb < 0 and vRel < 0 and (CS.vEgo*CV.MS_TO_KPH) <= 100:
-        multiplier2 = interp(abs(vRel*3.6), [1, 20], [1.1, 2.5])
+        multiplier2 = interp(abs(vRel*3.6), [1, 20], [1.1, 3])
         output_gb *= multiplier2
 
       if prevent_overshoot:
@@ -177,7 +177,7 @@ class LongControl():
       # Keep applying brakes until the car is stopped
       factor = 1
       if hasLead:
-        factor = interp(dRel,[2.0,3.0,4.0,5.0,6.0,7.0,8.0], [5,3,1.2,0.7,0.5,0.3,0.0])
+        factor = interp(dRel,[2.0,3.0,4.0,5.0,6.0,7.0,8.0], [6,3.5,1.3,0.7,0.5,0.3,0.0])
       if not CS.standstill or output_gb > -BRAKE_STOPPING_TARGET:
         output_gb -= CP.stoppingBrakeRate / RATE * factor
       output_gb = clip(output_gb, -brake_max, gas_max)
