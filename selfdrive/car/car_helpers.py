eadda7aabc3fa2561b754448fd59138051502dc0
Vehicle Researcher <>
2021-01-17 12:58:19 +0900

    openpilot v0.8.1

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -181,7 +181,7 @@ def get_car(logcan, sendcan):
     candidate = "mock"
 
   if Params().get("CarModel", encoding="utf8") != "":
-    candidate = Params().get("CarModel", encoding="utf8")
+    candidate = Params().get("CarModel", encoding="utf8", end='')
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, car_fw)
