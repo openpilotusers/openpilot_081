fdc035eb5fbb08c6c9bca4d8cad821bf9f271864
Vehicle Researcher <>
2021-01-18 14:19:59 +0900

    openpilot v0.8.1

--- common/apk.py
+++ common/apk.py
@@ -6,7 +6,7 @@ import shutil
 from common.basedir import BASEDIR
 from selfdrive.swaglog import cloudlog
 
-android_packages = ("com.mixplorer", "kr.mappers.AtlanSmart", "ai.comma.plus.offroad",)
+android_packages = ("com.mixplorer", "kr.mappers.AtlanSmart", "com.gmd.hidesoftkeys", "com.google.android.inputmethod.korean", "ai.comma.plus.offroad",)
 
 def get_installed_apks():
   dat = subprocess.check_output(["pm", "list", "packages", "-f"], encoding='utf8').strip().split("\n")
