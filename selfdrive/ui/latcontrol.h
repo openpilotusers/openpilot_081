e223e79eabc8d84085e098a4658922a71953ff22
Vehicle Researcher <>
2021-01-29 08:59:49 +0900

    Revert "openpilot v0.8.1"
    This reverts commit a9b9c1061d39c44abf43e697c93e3ae5945f3c9b.

--- selfdrive/ui/latcontrol.h
+++ selfdrive/ui/latcontrol.h
@@ -17,15 +17,6 @@ bool control_button_clicked1(int touch_x, int touch_y) {
   return false;
 }
 
-bool control_button_clicked2(int touch_x, int touch_y) {
-  if (touch_x >= 1000 && touch_x <= 1100) {
-    if (touch_y >= 945 && touch_y <= 1045) {
-      return true;
-    }
-  }
-  return false;
-}
-
 bool control_button_screenshot(int touch_x, int touch_y) {
   if (touch_x >= 0 && touch_x <= 150) {
     if (touch_y >= 465 && touch_y <= 615) {
@@ -64,13 +55,6 @@ bool latcontrol( UIState *s, int touch_x, int touch_y ) {
     Params().write_db_value("LimitSetSpeed", "1", 1);
     //system("su -c 'am start -n com.gmd.hidesoftkeys/com.gmd.hidesoftkeys.MainActivity'");
     system("su -c 'am start --activity-task-on-home com.skt.tmap.ku/com.skt.tmap.activity.TmapNaviActivity'");
-    s->sound->play(AudibleAlert::CHIME_WARNING1);
-    touched = true;
-  }
-
-  if ((control_button_clicked2(touch_x,touch_y)) && (s->status != STATUS_OFFROAD)) {
-    Params().write_db_value("LimitSetSpeed", "0", 1);
-    system("su -c 'am start --activity-task-on-home ai.comma.plus.offroad/.MainActivity'");
     s->sound->play(AudibleAlert::CHIME_WARNING1);
     touched = true;
   }
